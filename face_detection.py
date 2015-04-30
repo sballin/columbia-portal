06b740a4a0183d02dad5914ffe124647b43eca80
Sean Ballinger <sean.ballinger@gmail.com>
2015-04-30 06:02:43 +0000

    sunglasses, speedup, better text overlay

--- face_detection.py
+++ face_detection.py
@@ -1,41 +1,48 @@
-import sys, cv2
+import sys 
+import cv2
 import subprocess
 import textwrap
 import pygame
 import picamera
+import picamera.array
 import numpy as np
-import io
-import pickle
 
 
 cascade = cv2.CascadeClassifier('classifiers/opencv_frontalface.xml')
 
 
-def cascade_detect(cascade, image):
+def cascade_detect(cascade, image, minSide):
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     return cascade.detectMultiScale(
         gray_image,
         scaleFactor = 1.15,
         minNeighbors = 5,
-        minSize = (70, 70),
+        minSize = (minSide, minSide),
         flags = cv2.cv.CV_HAAR_SCALE_IMAGE
     )
  
-    
-def draw_bubbles(image):
-    detections = cascade_detect(cascade, image)
- 
+
+def draw_sunglasses(image, detections):
     for (x, y, w, h) in detections:
-        print "({0}, {1}, {2}, {3})".format(x, y, w, h)
-        text = subprocess.check_output(['fortune', '-sa']) # check for too many newlines
-        text = textwrap.fill(text, 25)
-        print text
-        cv2.putText(image, text, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255,255,255))
-        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        #cv2.ellipse(image, (x, y), (w, h), 0, 0, 180)#, ['black', 2]) 
+        cv2.rectangle(image, (x, int(1.3*y)), (x + w, int(1.65*y)), (0, 0, 0), -1)
     if len(detections) == 0:
         cv2.putText(image, 'No faces found', (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255,255,255))
-    cv2.imwrite('../tests/test.jpg', image)
+    return image
+
+    
+def draw_thoughts(image, detections):
+    for (x, y, w, h) in detections:
+        text = subprocess.check_output(['fortune', '-sa']) 
+        text = textwrap.fill(text, 25)
+        lines = text.split('\n')
+        line_number = 0
+        cv2.rectangle(image, (x + w, y - 30), (x + w + 400, y + len(lines)*30), (0, 0, 0), -1)
+        #cv2.ellipse(image, (x, y), (w, h), 0, 0, 180)#, ['black', 2]) 
+        for line in lines:
+            cv2.putText(image, line, (x + w, y + line_number*30), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255,255,255))
+            line_number += 1
+    if len(detections) == 0:
+        cv2.putText(image, 'No faces found', (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255,255,255))
     return image
 
 
@@ -45,39 +52,33 @@ def draw_frames(image, detections):
         cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
  
 
-def frame_faces(image_path, result_path):
-    image = cv2.imread(image_path)
-    if image is None:
-        print "ERROR: Image did not load."
-        return 2
-
-    detections = cascade_detect(cascade, image)
-    draw_frames(image, detections)
- 
-    cv2.imwrite(result_path, image)
-
-
 image_size = (800, 600)
 #img = np.ones(img_size)*255
 
 pygame.init()
-screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+screen = pygame.display.set_mode(image_size, pygame.FULLSCREEN)
 
-with picamera.PiCamera() as camera:
-    try:
-        camera.resolution = image_size
-        stream = io.BytesIO()
-    
-        for foo in camera.capture_continuous(stream, format='png'):
-            frame = np.fromstring(stream.getvalue(), dtype = np.uint8)
-            stream.truncate()
-            stream.seek(0)
-            image = cv2.imdecode(frame, 1)
-            image = draw_bubbles(image)
-            image = pygame.image.load('../tests/test.jpg')
-            #surf = pygame.surfarray.make_surface(image)    # note: makes colors bluish
-            screen.blit(image, (0,0))
-            pygame.display.flip()
+camera = picamera.PiCamera()
+camera.resolution = image_size
+with picamera.array.PiRGBArray(camera) as stream:
+    while True:
+        camera.capture(stream, 'rgb', use_video_port=True)
+        image = stream.array
+        detections = cascade_detect(cascade, image, 70)
+        image = draw_sunglasses(image, detections)
+        surf = pygame.surfarray.make_surface(image)
+        surf = pygame.transform.rotate(surf, -90)
+        surf = pygame.transform.flip(surf, 1, 0)
+        screen.blit(surf, (0,0))
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: break
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    break
+        stream.seek(0)
+        stream.truncate()
 
-    finally: camera.close()
+camera.close()
 
